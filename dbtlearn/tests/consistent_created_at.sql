db935b2da56e976aa8ef1fc8c0cc88e365d5b04c
Afaque Ahmad <afaque.ahmad@quantumblack.com>
2022-12-23 08:12:59 +0530

    Removed ';'

--- dbtlearn/tests/consistent_created_at.sql
+++ dbtlearn/tests/consistent_created_at.sql
@@ -1,7 +1,6 @@
-SELECT 
-    * 
+SELECT * 
 FROM {{ ref('fct_reviews') }} R
 JOIN {{ ref('dim_listings_cleansed') }} L
 ON R.LISTING_ID = L.LISTING_ID
 WHERE L.CREATED_AT >= R.REVIEW_DATE
-LIMIT 5;
+LIMIT 5
